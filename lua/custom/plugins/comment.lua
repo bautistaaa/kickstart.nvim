8bb7682a2694056d84736cb21820fa333a4747aa
bautistaaa <chrisbautistaaa@gmail.com>
2026-07-16 19:58:41 -0700

    fix: comments in env files

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -2,8 +2,12 @@ return {
   'numToStr/Comment.nvim',
   dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
   config = function()
+    local context_commentstring = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+
     require('Comment').setup {
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      pre_hook = function(ctx)
+        return context_commentstring(ctx) or vim.bo.commentstring
+      end,
     }
   end,
 }
